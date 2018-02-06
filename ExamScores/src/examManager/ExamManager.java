d28c520e66d31200c6b8a6c714d0d4cdeaebe093
anderson <anderson@java-devops>
2018-02-06 14:48:17 -0500

    Re-added added the project ExamScores and restarting the chapter 13

--- ExamScores/src/examManager/ExamManager.java
+++ ExamScores/src/examManager/ExamManager.java
@@ -2,17 +2,10 @@ package examManager;
 
 import java.util.ArrayList;
 import java.util.List;
-//import java.util.function.Function;
-import java.util.function.ToDoubleFunction;
+
+import calculators.CalculationFunctionInterface;
 
 public class ExamManager {
-	
-	// The simplest approach is using a java predefined interface from java.util.function
-	
-	// Another approach could be using a developer defined interface
-//	public interface CalculationFunctionInterface {
-//		public Double execute(List<Double> scores);
-//	}
 
 	private List<Double> myScores;
 
@@ -27,18 +20,7 @@ public class ExamManager {
 		myScores.add(54.1);
 	}
 
-	// Using a developer defined interface
-//	public Double customCalculation(CalculationFunctionInterface calculator) {
-//		return calculator.execute(myScores);
-//	}
-	
-	// Using the generic java predefined interface from java.util.function 
-//	public Double customCalculation(Function<List<Double>, Double> calculator) {
-//		return calculator.apply(myScores);
-//	}
-	
-	// Using a specific java predefined interface from java.util.function 
-	public Double customCalculation(ToDoubleFunction<List<Double>> calculator) {
-		return calculator.applyAsDouble(myScores);
+	public Double customCalculation(CalculationFunctionInterface calculator) {
+		return calculator.execute(myScores);
 	}
 }
