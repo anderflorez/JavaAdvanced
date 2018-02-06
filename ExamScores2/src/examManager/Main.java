0dc1bde6b7db0773ee2a3dd4d02310bf2ad984af
anderson <anderson@java-devops>
2018-02-06 15:54:00 -0500

    Removed the defined interface to use a java predefined interface

--- ExamScores2/src/examManager/Main.java
+++ ExamScores2/src/examManager/Main.java
@@ -1,10 +1,13 @@
 package examManager;
 
+import java.util.List;
+import java.util.function.Function;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		ExamManager.CalculationFunctionInterface average = myScores -> {
+		Function<List<Double>, Double> average = myScores -> {
 			Double sum = 0d;
 			
 			for (Double nextScore : myScores) {
@@ -13,7 +16,7 @@ public class Main {
 			return sum / myScores.size();
 		};
 		
-		ExamManager.CalculationFunctionInterface highestScore = myScores -> {
+		Function<List<Double>, Double> highestScore = myScores -> {
 			Double highest = 0d;
 			for (Double nextScore : myScores) {
 				highest = Math.max(highest, nextScore);
