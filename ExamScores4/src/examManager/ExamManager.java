ff049d9cebfeb3cedda461db01540639f194f14c
anderson <anderson@java-devops>
2018-02-08 08:54:16 -0500

    Duplicated ExamScores3 to 4 for Lambdas with collections chapter

---
+++ ExamScores4/src/examManager/ExamManager.java
@@ -0,0 +1,25 @@
+package examManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.ToDoubleFunction;
+
+public class ExamManager {
+	
+	private List<Double> myScores;
+
+	public ExamManager() {
+		myScores = new ArrayList<Double>();
+		myScores.add(78.3);
+		myScores.add(69.2);
+		myScores.add(44.7);
+		myScores.add(61.6);
+		myScores.add(80.2);
+		myScores.add(66.9);
+		myScores.add(54.1);
+	}
+
+	public Double customCalculation(ToDoubleFunction<List<Double>> calculator) {
+		return calculator.applyAsDouble(myScores);
+	}
+}
